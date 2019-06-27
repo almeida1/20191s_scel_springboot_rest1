9d46f2c2d65b01d60977656a80b7b1ff53cbb58f
HP <HP@DV6-6170>
2019-06-27 11:17:51 -0300

    Aula12 inclusao da classe de dominio Aluno

--- scel/src/main/java/com/fatec/scel/controller/AlunoController.java
+++ scel/src/main/java/com/fatec/scel/controller/AlunoController.java
@@ -1,16 +1,27 @@
 package com.fatec.scel.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.scel.model.Aluno;
 
 @RestController
 @RequestMapping(value="/alunos")
 public class AlunoController {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O rest esta funcionando";
+	public List<Aluno> listar() {
+		
+		Aluno aluno1 = new Aluno (1,"1111","jose da silva", "jose@gmail.com");
+		Aluno aluno2 = new Aluno (2,"1111","jose da silva", "jose@gmail.com");
+		List<Aluno> lista = new ArrayList<>();
+		lista.add(aluno1);
+		lista.add(aluno2);
+		return lista;
 	}
 
 }
---
+++ scel/src/main/java/com/fatec/scel/model/Aluno.java
@@ -0,0 +1,86 @@
+package com.fatec.scel.model;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String ra;
+	private String nome;
+	private String email;
+
+	public Aluno() {
+	}
+
+	public Aluno(Integer id, String ra, String nome, String email) {
+		this.id = id;
+		this.ra = ra;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (ra == null) {
+			if (other.ra != null)
+				return false;
+		} else if (!ra.equals(other.ra))
+			return false;
+		return true;
+	}
+}
