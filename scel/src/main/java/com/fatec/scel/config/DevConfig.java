375acfbbdfa029603b8864651d039e93ab41565c
edson <edson@192.168.0.5>
2019-07-28 18:12:05 -0300

    Aula 54 - Criando o profile de desenvolvimento

--- scel/src/main/java/com/fatec/scel/config/DevConfig.java
+++ scel/src/main/java/com/fatec/scel/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.fatec.scel.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,17 +11,21 @@ import org.springframework.context.annotation.Profile;
 import com.fatec.scel.services.DBService;
 
 @Configuration
-@Profile("dev")
+@Profile("test")
 
-public class TestConfig {
+public class DevConfig {
 	@Autowired
 	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String estrategiaDeInstanciacaoDoDB; //none nao recria o db
+
 	@Bean
 	public boolean instanticiaTestDataBase() throws ParseException {
+		if (!"create".equals(estrategiaDeInstanciacaoDoDB)) {
+			return false;
+		}
 		dbService.instanciaDB();
      	return true;
-
 	}
-
-
 }
